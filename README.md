d18b102c8893fce156193c2dc46c73b1e777a0aa
Bart Nagel <bart@tremby.net>
2020-07-14 01:40:36 -0700

    Rewrite

--- README.md
+++ README.md
@@ -1,39 +1,4 @@
 HTML5 dice
 ==========
 
-Build
------
-
-Assuming `npm` is installed, install the Grunt tool and bower globally:
-
-	sudo npm install -g grunt-cli bower
-
-Install dependencies:
-
-	npm install
-	bower install
-
-Build:
-
-	grunt
-
-Watch stylus and coffeescript sources and rebuild when they change:
-
-	grunt watch
-
-The watch command does not roll the libraries together. To do just that:
-
-	grunt uglify:libs
-
-Todo
-----
-
-- Fix SVG dots which don't appear on old Android
-- Make sound optional
-- Option to shake device to roll
-- Fix roll history dialog which is dodgy on some browsers
-
-Licence
--------
-
-MIT
+Simple dice.
---
+++ dice.js
@@ -0,0 +1,64 @@
+const scene = document.querySelector('#scene');
+const numDiceInput = document.querySelector('input[name="numDice"]');
+const sound = document.querySelector('#sound');
+const rollHistoryList = document.querySelector('#history ol');
+
+function updateNumDice() {
+	const newNumDice = parseInt(numDiceInput.value);
+	const oldNumDice = scene.children.length;
+	while (scene.children.length > newNumDice) {
+		scene.children[newNumDice].remove();
+	}
+	while (scene.children.length < newNumDice) {
+		scene.appendChild(scene.children[0].cloneNode(true));
+	}
+}
+numDiceInput.addEventListener('change', updateNumDice);
+updateNumDice();
+
+let rollCount = 0;
+function roll() {
+	scene.dataset.rollCountMod5 = ++rollCount % 5;
+	const rolls = [];
+	for (const die of scene.querySelectorAll('.die')) {
+		const roll = Math.floor(Math.random() * 6) + 1;
+		rolls.push(roll);
+		die.dataset.result = roll;
+	}
+
+	addToHistory(rolls);
+
+	if (document.querySelector('input[name="sound"]').checked) {
+		sound.currentTime = 0;
+		sound.play();
+	}
+}
+scene.addEventListener('click', roll);
+document.body.addEventListener('keydown', (event) => {
+	if (event.key === ' ') {
+		event.preventDefault();
+		roll();
+	}
+});
+
+function addToHistory(rolls) {
+	const li = document.createElement('li');
+	const strong = document.createElement('strong');
+	strong.appendChild(document.createTextNode(rolls.reduce((acc, roll) => acc + roll, 0)));
+	li.appendChild(strong);
+	li.appendChild(document.createTextNode(` (${rolls.join(', ')})`));
+	rollHistoryList.appendChild(li);
+	li.scrollIntoView();
+}
+
+function updateNumberStyle() {
+	scene.dataset.numberStyle = document.querySelector('input[name="numberStyle"]:checked').value;
+}
+for (const radio of document.querySelectorAll('input[name="numberStyle"]')) {
+	radio.addEventListener('change', updateNumberStyle);
+}
+updateNumberStyle();
+
+document.querySelector('#fullscreen').addEventListener('click', () => {
+	document.querySelector('#fullscreenArea').requestFullscreen();
+});
