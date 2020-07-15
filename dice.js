b358fa23e79260ab9e34fa48e4a07c7400ccf492
Bart Nagel <bart@tremby.net>
2020-07-14 17:18:57 -0700

    Add shake to roll option
    The device motion API is deprecated since it can be used for tracking,
    but this adds support where it's available.
    
    The code isn't perfect -- it'd be better to determine the vector of
    movement and see that that gets reversed a few times rather than looking
    at each cardinal direction and rotation axis individually -- but it does
    the job.
    
    Maybe in future this will be useful again, if browsers implement an
    opt-in mechanism for device motion per website, like for location
    details etc.

--- dice.js
+++ dice.js
@@ -77,3 +77,91 @@ if (!fullscreenArea.requestFullscreen && !fullscreenArea.webkitRequestFullscreen
 		}
 	});
 }
+
+const shakeCheckbox = document.querySelector('input[name="shake"]');
+if (window.DeviceMotionEvent) {
+	const ACCELERATION_THRESHOLD_RANGE = [8, 14];
+	const ROTATION_THRESHOLD_RANGE = [40, 80];
+	const TURNS_PER_SECOND_THRESHOLD = 5;
+	const SHAKE_COOLOFF = 1e3;
+
+	let lastShakeTime = null;
+
+	const turns = {
+		// Changes of linear acceleration direction
+		x: [],
+		y: [],
+		z: [],
+
+		// Changes of rotation direction
+		alpha: [],
+		beta: [],
+		gamma: [],
+	};
+
+	function handleMotion(event) {
+		const time = new Date();
+		const minTime = time - 1e3;
+
+		// See if we've changed direction in any of the six dimensions
+		for (const [dim, value, thresholdRange] of [
+			['x', event.acceleration.x, ACCELERATION_THRESHOLD_RANGE],
+			['y', event.acceleration.y, ACCELERATION_THRESHOLD_RANGE],
+			['z', event.acceleration.z, ACCELERATION_THRESHOLD_RANGE],
+			['alpha', event.rotationRate.alpha, ROTATION_THRESHOLD_RANGE],
+			['beta', event.rotationRate.alpha, ROTATION_THRESHOLD_RANGE],
+			['gamma', event.rotationRate.gamma, ROTATION_THRESHOLD_RANGE]
+		]) {
+			// Abort if it's outside the threshold range
+			if (Math.abs(value) < thresholdRange[0] || Math.abs(value) > thresholdRange[1]) continue;
+
+			// Abort if direction is the same as the last recording
+			if (turns[dim].length > 0) {
+				const prevValue = turns[dim][turns[dim].length - 1].value;
+				if (value > 0 && prevValue > 0 || value < 0 && prevValue < 0) {
+					// Same direction
+					continue;
+				}
+			}
+
+			// Remove old or irrelevant direction changes
+			if (turns[dim].length > TURNS_PER_SECOND_THRESHOLD) {
+				turns[dim] = turns[dim].slice(-TURNS_PER_SECOND_THRESHOLD);
+			}
+			for (let i = turns[dim].length - 1; i >= 0; i--) {
+				if (turns[dim][i].time <= minTime) {
+					turns[dim] = turns[dim].slice(i);
+					break;
+				}
+			}
+
+			// Log the direction change
+			turns[dim].push({
+				time,
+				value,
+			});
+
+			// Has it been long enough since the last shake?
+			if (lastShakeTime != null && time - lastShakeTime < SHAKE_COOLOFF) continue;
+
+			// Have we shaken fast enough?
+			if (turns[dim].length > TURNS_PER_SECOND_THRESHOLD) {
+				roll();
+				lastShakeTime = time;
+				break;
+			}
+		}
+	}
+
+
+	shakeCheckbox.addEventListener('change', () => {
+		if (shakeCheckbox.checked) {
+			window.addEventListener('devicemotion', handleMotion);
+		} else {
+			window.removeEventListener('devicemotion', handleMotion);
+		}
+	});
+} else {
+	shakeCheckbox.disabled = true;
+	shakeCheckbox.closest('li').append(" (not available on this browser)");
+}
